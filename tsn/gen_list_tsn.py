1fcc67f6836ddcbd46d8542119693848044bfbd7
/media/remote_home/mtriet <you@example.com>
2018-05-02 15:07:07 +0200

    tsn copy files

---
+++ tsn/gen_list_tsn.py
@@ -0,0 +1,59 @@
+# proposal needs to see the all augemented data
+
+from itertools import islice
+import numpy as np
+import sys
+import pdb
+import os
+import json
+from subtitle import Subtitle
+import util
+
+FRAME_PATH = "/media/data/mtriet/dataset/scnn_%s_frames" % sys.argv[1]
+SUB_PATH = "/media/data/mtriet/raw_video/%s/train" % (sys.argv[1])
+WINDOW_SIZE = [16,32,64,128,256,512] 
+OVERLAP_RATE = 0.75
+CLASSES = [[], []]  # 0, 1
+
+if len(sys.argv) < 3:
+  print('fb pad=True/False')
+  sys.exit(1)
+  
+for window_size in WINDOW_SIZE:
+  for frame_root, sub_folder, sub_files in os.walk(FRAME_PATH):
+    for folder in sub_folder:       
+      subtitles = Subtitle.load_subtitle(SUB_PATH, folder, sys.argv[2]) 
+      frames = sorted(os.listdir(frame_root + '/' + folder))
+      sub_index = 0
+      for begin_pivot in range(1, len(frames) - window_size, int(window_size*(1 - OVERLAP_RATE))):  # ignore last few frames
+        if (begin_pivot > subtitles[sub_index].end) and (sub_index < len(subtitles) - 1):
+          sub_index += 1 
+
+        end_pivot = min(begin_pivot + window_size, len(frames))
+        segment = range(begin_pivot, begin_pivot + window_size + 1)
+        sub_range = subtitles[sub_index].get_range()
+        intersection = np.intersect1d(segment, sub_range)
+        if len(intersection) == len(segment):        # the subtitle contains the segment
+          CLASSES[1].append("%s/%s/ %06d %d %d\n" % (frame_root, folder, begin_pivot, 1, window_size/16))    
+        elif len(intersection) == 0:        # not contains at all 
+          CLASSES[0].append("%s/%s/ %06d %d %d\n" % (frame_root, folder, begin_pivot, 0, window_size/16))    
+        else:
+          union = len(segment) + len(sub_range) - len(intersection)
+          current_score = 1.0 * len(intersection) / union 
+          if current_score > 0.7:
+            CLASSES[1].append("%s/%s/ %06d %d %d\n" % (frame_root, folder, begin_pivot, 1, window_size/16))    
+          else:  # check if next segment has better score, if > 0.7, and if max of them > 0.5
+            if sub_index == len(subtitles) - 1: # already at last subtitle
+              CLASSES[0].append("%s/%s/ %06d %d %d\n" % (frame_root, folder, begin_pivot, 0, window_size/16))    
+            else:
+              sub_range = subtitles[sub_index+1].get_range()
+              intersection = np.intersect1d(segment, sub_range)
+              next_score = 1.0 * len(intersection) / union 
+              if max(current_score, next_score) > 0.5:
+                CLASSES[1].append("%s/%s/ %06d %d %d\n" % (frame_root, folder, begin_pivot, 1, window_size/16))    
+              else:
+                CLASSES[0].append("%s/%s/ %06d %d %d\n" % (frame_root, folder, begin_pivot, 0, window_size/16))
+
+CLASSES = util.balance(CLASSES)
+(x, y) = util.to_xy(CLASSES)
+util.print_data('scnn_%s_train_proposal.lst' % sys.argv[1], 'scnn_%s_val_proposal.lst' % sys.argv[1], x, y)
---
+++ tsn/step_1_split_frames_from_sub.py
@@ -0,0 +1,53 @@
+# after extracting the flow, use this file to split the frames to appropirate folders
+# gen_list_tsn next
+
+import itertools
+import pdb
+import os
+import sys
+import subprocess
+import shutil
+
+    
+# input: vid_path = video path, allows wildcard
+# output: folders, each contains all frames of the video
+def dump_frames(sport, pad):
+  VID_PATH = "/media/data/mtriet/raw_video/%s/train/" % sport 
+  OUTPUT = '/media/data/mtriet/dataset/tsn_%s_frames/' % sport
+  ORIGINAL_FRAMES = '/media/data/mtriet/dataset/backup/tsn_%s_frames/train/' % sport
+
+  for f in os.listdir(VID_PATH): 
+    if f.endswith(".aqt"):
+      if (pad and f.endswith('_pad.aqt')) or (not pad and not f.endswith('_pad.aqt')):
+        vid_name = f[:-4] if not pad else f[:-8]  # remove extension
+        subtitle_path = os.path.dirname(VID_PATH) + '/' + vid_name + ".aqt"
+        
+        with open(subtitle_path, 'r') as f:
+          index = 0
+          while True:
+            index = index + 1
+            next_n_lines = list(itertools.islice(f, 4))
+            if not next_n_lines:
+              break
+            
+            next_n_lines[0] = int(next_n_lines[0].split(' ')[1])
+            next_n_lines[2] = int(next_n_lines[2].split(' ')[1])
+            sub_folder = '_'.join([vid_name,str(index),next_n_lines[1].strip()])
+            output_frame_path = OUTPUT + '/' + sub_folder + '/' 
+
+            try:
+              os.mkdir(output_frame_path)
+            except:
+              shutil.rmtree(output_frame_path)
+              os.mkdir(output_frame_path)
+
+            for num, value in enumerate(range(next_n_lines[0], next_n_lines[2])):
+              shutil.copyfile("%s/%s/flow_x_%05d.jpg" % (ORIGINAL_FRAMES, vid_name, value), "%s/flow_x_%05d.jpg" % (output_frame_path, num))
+              shutil.copyfile("%s/%s/flow_y_%05d.jpg" % (ORIGINAL_FRAMES, vid_name, value), "%s/flow_y_%05d.jpg" % (output_frame_path, num))
+              shutil.copyfile("%s/%s/image_%05d.jpg" % (ORIGINAL_FRAMES, vid_name, value), "%s/image_%05d.jpg" % (output_frame_path, num))
+ 
+if __name__ == "__main__":
+  if len(sys.argv) < 3:
+    print 'gen_frame <sport> <pad (T/F)>'
+    sys.exit(1) 
+  dump_frames(sys.argv[1], sys.argv[2]) 
